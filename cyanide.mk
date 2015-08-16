10c85db12c0eca7532f1c71adb146e3f8aceadbc
rogersb11 <brettrogers11@gmail.com>
2015-08-16 13:03:43 -0400

    Cyanide

---
+++ cyanide.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
+
+# Inherit some common CYANIDE stuff.
+$(call inherit-product, vendor/cyanide/config/common_full_tablet_wifionly.mk)
+
+PRODUCT_DEVICE := mondrianwifi
+PRODUCT_NAME := cyanide_mondrianwifi
